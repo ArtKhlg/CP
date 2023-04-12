ba0166e9d5ad0f996624363f4e5f42a099dca501
ArtKhlg <shhessa@bk.ru>
2023-04-12 16:24:34 +0300

    homework auth1

---
+++ src/app/hooks/useLogin.jsx
@@ -0,0 +1,74 @@
+import React, { useContext, useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { setTokens } from "../services/localStorage.service";
+
+const httpLogIn = axios.create();
+
+const LogInContext = React.createContext();
+
+export const useLogIn = () => {
+    return useContext(LogInContext);
+};
+
+const LogInProvider = ({ children }) => {
+    const [currentUser] = useState({});
+    const [error, setError] = useState(null);
+
+    async function signIn({ email, password }) {
+        const url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.REACT_APP_FIREBASE_KEY}`;
+        try {
+            const { data } = await httpLogIn.post(url, {
+                email,
+                password,
+                returnSecureToken: true
+            });
+            setTokens(data);
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+            errorCatcher(error);
+            const { code, message } = error.response.data.error;
+            if (code === 400) {
+                if (message === "INVALID_PASSWORD") {
+                    const errorObject = {
+                        password: "Неверный пароль"
+                    };
+                    throw errorObject;
+                }
+                if (message === "EMAIL_NOT_FOUND") {
+                    const errorObject = {
+                        email: "Неверный email"
+                    };
+                    throw errorObject;
+                }
+            }
+        }
+    }
+
+    useEffect(() => {
+        if (error !== null) {
+            toast(error);
+            setError(null);
+        }
+    }, [error]);
+
+    function errorCatcher(error) {
+        const { message } = error.response.data;
+        setError(message);
+    }
+    return (
+        <LogInContext.Provider value={{ signIn, currentUser }}>
+            {children}
+        </LogInContext.Provider>
+    );
+};
+LogInProvider.propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node
+    ])
+};
+
+export default LogInProvider;
